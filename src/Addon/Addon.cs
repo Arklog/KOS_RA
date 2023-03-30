7c5705651cc95461f5dce7776bfcb56c5cf4064f
Arklog <pierr.ducloux@gmail.com>
2023-03-30 12:44:16 +0200

    First commit

---
+++ src/Addon/Addon.cs
@@ -0,0 +1,72 @@
+using kOS.Safe.Encapsulation;
+using kOS.Safe.Encapsulation.Suffixes;
+using kOS.Suffixed;
+using kOS.Suffixed.Part;
+using System;
+using System.Linq;
+using System.Reflection;
+using FinePrint.Utilities;
+using kOS;
+using kOS.AddOns;
+using RealAntennas;
+using RealAntennas.Targeting;
+using UnityEngine;
+
+
+namespace KOS_RA.Addon
+{
+    [kOSAddon("RA")]
+    [kOS.Safe.Utilities.KOSNomenclature("RAAddon")]
+    public class Addon: kOS.Suffixed.Addon
+    {
+        public Addon(SharedObjects shared) : base(shared)
+        {
+            InitializeSuffixes();    
+        }
+
+        private void InitializeSuffixes()
+        {
+            AddSuffix("HASCONNECTION", new OneArgsSuffix<BooleanValue, VesselTarget>(RAHasConnection, "Check if given vessel have a connection"));
+            AddSuffix("SETTARGETBODY", new TwoArgsSuffix<PartValue, StringValue>(RASetTargetBody, "Set an real antenna target"));
+            AddSuffix("SETTARGETVESSEL", new TwoArgsSuffix<PartValue, VesselTarget>(RASetTargetVessel, "Set an real antenna target"));
+        }
+        
+        public override BooleanValue Available()
+        {
+            return true;
+        }
+
+        private BooleanValue RAHasConnection(VesselTarget target)
+        {
+            return target.Vessel.connection.IsConnected;
+        }
+
+        private void RASetTargetBody(PartValue part, StringValue target)
+        {
+            var module = part.Part.FindModuleImplementing<ModuleRealAntenna>();
+            var body = FlightGlobals.GetBodyByName(target);
+            
+            if (module != null && body != null)
+            {
+                var antennaTargetNode = new ConfigNode(AntennaTarget.nodeName);
+                antennaTargetNode.AddValue("name", $"{AntennaTarget.TargetMode.BodyLatLonAlt}");
+                antennaTargetNode.AddValue("bodyName", body.name);
+                antennaTargetNode.AddValue("latLonAlt", new Vector3(0, 0, (float)-body.Radius));
+                module.Target = AntennaTarget.LoadFromConfig(antennaTargetNode, module.RAAntenna);
+            }
+        }
+
+        private void RASetTargetVessel(PartValue part, VesselTarget target)
+        {
+            var module = part.Part.FindModuleImplementing<ModuleRealAntenna>();
+
+            if (module != null)
+            {
+                var antennaTargetNode = new ConfigNode(AntennaTarget.nodeName);
+                antennaTargetNode.AddValue("name", $"{AntennaTarget.TargetMode.Vessel}");
+                antennaTargetNode.AddValue("vesselId", target.Vessel.id);
+                module.Target = AntennaTarget.LoadFromConfig(antennaTargetNode, module.RAAntenna);
+            }
+        }
+    }
+}
---
+++ src/Addon/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("KOS_RA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KOSRA")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
+[assembly: KSPAssemblyDependency("kOS", 1, 0)]
+[assembly: KSPAssemblyDependency("RealAntennas", 2, 0)]
